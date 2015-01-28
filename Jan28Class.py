f0a905df114a0b43bb9dd204c058c49898187737
bprest <brian.prest@duke.edu>
2015-01-28 15:25:05 -0500

    1/28

---
+++ Jan28Class.py
@@ -0,0 +1,11 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+main_dir = "\Users\bcp17\OneDrive\Grad School\2015-Spring\Big Data\data\raw"
+git_dir = "\Users\bcp17\OneDrive\Grad School\GitHub\PubPol590"
+main_dir = "/Users/bcp17/OneDrive/Grad School/2015-Spring/Big Data/data/raw"
+git_dir = "/Users/bcp17/OneDrive/Grad School/GitHub/PubPol590"
+#cd 'main_dir'
+#cd 'C:/Users/bcp17/OneDrive/Grad School/GitHub/PubPol590'
+
